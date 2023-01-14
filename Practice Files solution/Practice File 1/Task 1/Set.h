b8c398f8a260c65dce8885bf3fc9b0aaf883bd4e
Imran Javed <imranjaved1073@gmail.com>
2023-01-14 19:05:25 +0500

    Practice 1 Task 1

---
+++ Practice Files solution/Practice File 1/Task 1/Set.h
@@ -0,0 +1,244 @@
+#ifndef SET_H
+#define SET_H
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class Set
+{
+	T* data;
+	int capacity;
+	int noOfElements;
+	void resize(int newCapacity);
+	int searchElementIndex(const T& element) const;
+public:
+	Set();
+	~Set();
+	Set(const Set<T>&);
+	int getCardinality() const;
+	Set<T>& operator=(const Set<T>&);
+	bool insert(T);
+	bool remove(T);
+	bool isMember(T) const;
+	bool isSubset(const Set<T>&) const;
+
+	Set calcUnion(const Set&) const;
+	Set calcIntersection(const Set&) const;
+	Set calcDifference(const Set&) const;
+	Set<T> operator+(const Set<T>&) const;
+	Set<T> operator-(const Set<T>&) const;
+	bool operator==(const Set<T>&) const;
+
+	void print() const;
+};
+
+template <typename T>
+void Set<T>::resize(int newCapacity)
+{
+	int newNoOfElements = newCapacity < noOfElements ? newCapacity : noOfElements;
+	if (newCapacity <= 0)
+	{
+		this->~Set();
+		return;
+	}
+	T* ptr = new T[newCapacity];
+	if (capacity > 0)
+	{
+		int size = newCapacity < capacity ? newCapacity : capacity;
+		for (int i = 0; i < size; i++)
+			ptr[i] = data[i];
+	}
+	this->~Set();
+	capacity = newCapacity;
+	noOfElements = newNoOfElements;
+	data = ptr;
+}
+
+template <typename T>
+Set<T>::Set()
+{
+	capacity = 0;
+	noOfElements = 0;
+	data = nullptr;
+}
+
+template <typename T>
+Set<T>::Set(const Set<T>& rhs)
+{
+	if (rhs.data == nullptr)
+	{
+		this->capacity = 0;
+		this->noOfElements = 0;
+		data = nullptr;
+		return;
+	}
+	capacity = rhs.capacity;
+	noOfElements = rhs.noOfElements;
+	data = new T[capacity];
+	for (int i = 0; i < capacity; i++)
+		data[i] = rhs.data[i];
+}
+
+template <typename T>
+Set<T>::~Set()
+{
+	if (data)
+	{
+		delete[] data;
+		data = nullptr;
+	}
+	capacity = 0;
+	noOfElements = 0;
+}
+
+template <typename T>
+int Set<T>::getCardinality() const
+{
+	return noOfElements;
+}
+
+template <typename T>
+int Set<T>::searchElementIndex(const T& element) const
+{
+	for (int i = 0; i < noOfElements; i++)
+		if (data[i] == element)
+			return i;
+	return -1;
+}
+
+template <typename T>
+bool Set<T>::insert(T item)
+{
+	if (isMember(item))
+		return false;
+	if (noOfElements == capacity)
+		resize(capacity + 1);
+	data[noOfElements++] = item;
+	return true;
+}
+
+template <typename T>
+bool Set<T>::remove(T element)
+{
+	if (!isMember(element))
+		return false;
+	int index = searchElementIndex(element);
+	
+	for (int i = index; i < noOfElements - 1; i++)
+		data[i] = data[i + 1];
+	resize(noOfElements - 1);
+	return true;
+}
+
+template <typename T>
+bool Set<T>::isMember(T val) const
+{
+	return searchElementIndex(val) != -1;
+}
+
+template <typename T>
+Set<T> Set<T>::calcUnion(const Set& rhs) const
+{
+	Set temp;
+	for (int i = 0; i < noOfElements; i++)
+		temp.insert(data[i]);
+
+	for (int i = 0; i < rhs.noOfElements; i++)
+		temp.insert(rhs.data[i]);
+
+	return temp;
+}
+
+template <typename T>
+Set<T> Set<T>::calcIntersection(const Set<T>& rhs) const
+{
+	Set<T> temp;
+	for (int i = 0; i < noOfElements; i++)
+		if (rhs.isMember(data[i]))
+			temp.insert(data[i]);
+	return temp;
+}
+
+template <typename T>
+Set<T> Set<T>::calcDifference(const Set<T>& rhs) const
+{
+	Set<T> temp;
+	for (int i = 0; i < noOfElements; i++)
+		if (!rhs.isMember(data[i]))
+			temp.insert(data[i]);
+	return temp;
+}
+
+template <typename T>
+Set<T> Set<T>::operator+(const Set<T>& rhs) const
+{
+	return calcUnion(rhs);
+}
+
+template <typename T>
+Set<T> Set<T>::operator-(const Set<T>& rhs) const
+{
+	return calcDifference(rhs);
+}
+
+template <typename T>
+bool Set<T>::operator==(const Set<T>& rhs) const
+{
+	if (noOfElements != rhs.noOfElements)
+		return false;
+	for (int i = 0; i < noOfElements; i++)
+		if (!rhs.isMember(data[i]))
+			return false;
+	return true;
+}
+
+template <typename T>
+Set<T>& Set<T>::operator=(const Set<T>& rhs)
+{
+	if (this != &rhs)
+	{
+		this->~GenericArray();
+		if (ref.data == nullptr)
+		{
+			this->capacity = 0;
+			data = nullptr;
+			return *this;
+		}
+		capacity = rhs.capacity;
+		noOfElements = rhs.noOfElements;
+		data = new T[capacity];
+		for (int i = 0; i < capacity; i++)
+			data[i] = rhs.data[i];
+	}
+	return *this;
+}
+
+template <typename T>
+bool Set<T>::isSubset(const Set<T>& lhs) const
+{
+	int count = 0;
+	if (this->noOfElements > lhs.noOfElements)
+		return false;
+	for (int i = 0; i < noOfElements; i++)
+	{
+		if (lhs.isMember(data[i]))
+			count++;
+	}
+	if (count == this->noOfElements)
+		return true;
+	return false;
+}
+
+template <typename T>
+void Set<T>::print() const
+{
+	for (int i = 0; i < noOfElements; i++)
+		cout << data[i] << " ";
+	cout << endl;
+}
+
+
+
+#endif // !SET-H
---
+++ Practice Files solution/Practice File 1/Task 1/Source.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include "Set.h"
+
+using namespace std;
+
+void validSetNumber(int& a, int& b, int n)
+{
+	cout << "Enter first set: ";
+	cin >> a;
+	while (a > n)
+	{
+		cout << "Enter valid set number: ";
+		cin >> a;
+	}
+	cout << "Enter second set: ";
+	cin >> b;
+	while (b > n)
+	{
+		cout << "Enter valid set number: ";
+		cin >> b;
+	}
+}
+
+int main()
+{
+	cout << "How many sets you want to create? ";
+	int n;
+	cin >> n;
+	Set<int>* s = new Set<int>[n];
+	for (int i = 0; i < n; i++)
+	{
+		cout << "How many elements you want to insert in set " << i + 1 << "? ";
+		int m;
+		cin >> m;
+		for (int j = 0; j < m; j++)
+		{
+			int temp;
+			cout << "Enter element " << j + 1 << ": ";
+			cin >> temp;
+			s[i].insert(temp);
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Set " << i + 1 << ": ";
+		s[i].print();
+	}
+	int choice;
+	do
+	{
+		system("pause");
+		system("cls");
+		for (int i = 0; i < n; i++)
+		{
+			cout << "Set " << i + 1 << ": ";
+			s[i].print();
+		}
+		cout << "1. Union" << endl;
+		cout << "2. Intersection" << endl;
+		cout << "3. Difference" << endl;
+		cout << "4. Subset" << endl;
+		cout << "5. Equality" << endl;
+		cout << "6. Exit" << endl;
+		cout << "Enter your choice: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+		{
+			int a, b;
+			validSetNumber(a, b, n);
+			Set<int> temp = s[a - 1] + s[b - 1];
+			cout << "UNION of set " << a << " and set " << b << " is: ";
+			temp.print();
+			break;
+		}
+		case 2:
+		{
+			int a, b;
+			validSetNumber(a, b, n);
+			Set<int> temp = s[a - 1].calcIntersection(s[b - 1]);
+			cout << "INTERSECTION of set " << a << " and set " << b << " is: ";
+			temp.print();
+			break;
+		}
+		case 3:
+		{
+			int a, b;
+			validSetNumber(a, b, n);
+			Set<int> temp = s[a - 1] - s[b - 1];
+			cout << "DIFFERENCE of set " << a << " and set " << b << " is: ";
+			temp.print();
+			break;
+		}
+		case 4:
+		{
+			int a, b;
+			validSetNumber(a, b, n);
+			if (s[a - 1].isSubset(s[b - 1]))
+				cout << "Set " << a << " is SUBSET of set " << b << endl;
+			else
+				cout << "Set " << a << " is not SUBSET of set " << b << endl;
+			break;
+		}
+		case 5:
+		{
+			int a, b;
+			validSetNumber(a, b, n);
+			if (s[a - 1] == s[b - 1])
+				cout << "Set " << a << " is EQUAL to set " << b << endl;
+			else
+				cout << "Set " << a << " is NOT EQUAL to set " << b << endl;
+			break;
+		}
+		case 6:
+			break;
+		default:
+			cout << "Invalid choice" << endl;
+		}
+	} while (choice != 6);
+
+}
